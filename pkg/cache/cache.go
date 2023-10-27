bd1439aaf473b977e2ff77e33ea59d1329ae2bf8
Гришанов Клим <klim.grishanov@mail.ru>
2023-10-27 06:02:52 +0300

    Core: Create cache

---
+++ pkg/cache/cache.go
@@ -0,0 +1,54 @@
+package cache
+
+import (
+	"WB_Intern_L0/entity"
+	"WB_Intern_L0/internal/usecase"
+	"go.uber.org/zap"
+)
+
+type CacheService struct {
+	services *usecase.UseCase
+	Cache    map[string]entity.Order
+}
+
+func NewCache(services *usecase.UseCase) *CacheService {
+	return &CacheService{services: services, Cache: make(map[string]entity.Order)}
+}
+
+func (s *CacheService) NewOrder(order entity.Order) {
+	s.Cache[order.OrderUid] = order
+}
+
+func (s *CacheService) GetOrder(orderUID string) (entity.Order, bool) {
+	val, isExist := s.Cache[orderUID]
+	return val, isExist
+}
+
+func (s *CacheService) LoadDataFromDatabase() error {
+	orderArr, err := s.services.GetOrders()
+	if err != nil {
+		zap.L().Error(err.Error())
+		return err
+	}
+
+	for i := 0; i < len(orderArr); i++ {
+		Delivery, err := s.services.GetDeliveryByOrderUID(orderArr[i].OrderUid)
+		if err != nil {
+			return err
+		}
+		Payment, err := s.services.GetPaymentByOrderUID(orderArr[i].OrderUid)
+		if err != nil {
+			return err
+		}
+		Items, err := s.services.GetItemsByOrderUID(orderArr[i].OrderUid)
+		if err != nil {
+			return err
+		}
+		orderArr[i].Delivery = Delivery
+		orderArr[i].Payment = Payment
+		orderArr[i].Items = Items
+		s.Cache[orderArr[i].OrderUid] = orderArr[i]
+	}
+
+	return nil
+}
