852d5009f543d20a17b0f78f9afd0ae5c8809ba5
Гришанов Клим <klim.grishanov@mail.ru>
2023-10-27 06:04:38 +0300

    Core: Dependency Injection

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,15 +2,17 @@ package handler
 
 import (
 	"WB_Intern_L0/internal/usecase"
+	"WB_Intern_L0/pkg/cache"
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct {
+	cache    *cache.CacheService
 	services *usecase.UseCase
 }
 
-func NewHandler(services *usecase.UseCase) *Handler {
-	return &Handler{services: services}
+func NewHandler(services *usecase.UseCase, cache *cache.CacheService) *Handler {
+	return &Handler{services: services, cache: cache}
 }
 
 func (h *Handler) InitRoute() *fiber.App {
