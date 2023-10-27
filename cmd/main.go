c4c46625960e5e6cc3408790f821e843b3d65076
Гришанов Клим <klim.grishanov@mail.ru>
2023-10-27 06:05:06 +0300

    Core: Connect to NATS & Cache Service

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,12 @@ import (
 	"WB_Intern_L0/internal/handler"
 	"WB_Intern_L0/internal/repo"
 	"WB_Intern_L0/internal/usecase"
+	"WB_Intern_L0/pkg/cache"
+	natsService "WB_Intern_L0/pkg/nats"
 	"fmt"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/nats-io/nats.go"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"os"
@@ -41,7 +44,35 @@ func main() {
 
 	repository := repo.NewRepo(db)
 	services := usecase.NewUseCase(repository)
-	handlers := handler.NewHandler(services)
+	cacheService := cache.NewCache(services)
+	handlers := handler.NewHandler(services, cacheService)
+
+	// NATS Setup
+	nc, err := nats.Connect(nats.DefaultURL)
+
+	natsHandler := natsService.NewNats(services, nc, cacheService)
+
+	nc.Subscribe("newOrder", func(m *nats.Msg) {
+		natsHandler.CreateOrderNATS(m)
+	})
+
+	nc.Subscribe("getOrder", func(m *nats.Msg) {
+		natsHandler.GetOrderByIdNATS(m)
+	})
+
+	if err != nil {
+		zap.L().Error("nats connect error", zap.Error(err))
+		return
+	}
+
+	// Cache Setup
+	err = cacheService.LoadDataFromDatabase()
+	if err != nil {
+		zap.L().Error("cache reload error", zap.Error(err))
+		return
+	}
+
+	// Web Setup
 	srv := new(WB_Intern_L0.Server)
 
 	if err := srv.Run(viper.GetString("ip")+":"+viper.GetString("port"), handlers.InitRoute()); err != nil {
